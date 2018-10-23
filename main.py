b9754b8ec0b89d4cc12b013e9d72a52c90cee9d3
vedantc6 <vedantc6@gmail.com>
2018-10-23 18:33:29 -0400

    missing_values plot

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt 
 import numpy as np
-import functools
+import seaborn as sns
 
-DATA_DIR = os.getcwd() + "/Data/fma_metadata/"
+sns.set()
+
+DATA_DIR = os.getcwd() + "/Desktop/Projects/CS543-MusicGenreRecognition/Data/fma_metadata/"
 
 #%%
 genres_df = pd.read_csv(DATA_DIR + "genres.csv", encoding="latin-1", index_col=0) 
@@ -30,33 +32,27 @@ print(album_df.head())
 features_df = pd.read_csv(DATA_DIR + "features.csv", encoding="Latin-1", index_col=0, header=[0,1,2])
 print(features_df.head())
 #%%
-col_tracks = list(tracks_df.columns.values)
-#%%
-# Missing values in datasets
-def num_missing(x):
-    i = sum(x.isnull())
-    return (i/len(x))*100
-
-print("Missing values per column: ")
-print("Tracks dataset: ")
-missing_track_info, rm_tracks = tracks_df.apply(num_missing, axis = 0) 
-print(missing_track_info)
-print("\nGenres dataset: " )
-missing_genre_info = genres_df.apply(num_missing, axis = 0)
-print(missing_genre_info)
-print("\nEchonest dataset: " )
-missing_echo_info = echonest_df.apply(num_missing, axis = 0)
-print(missing_echo_info)
-print("\nFeatures dataset: " )
-missing_features_info = features_df.apply(num_missing, axis = 0)
-print(missing_features_info)
-
-# Delete columns which have more than 30% data missing
-
-
-#%%
 # Number of unique number of tracks, artists, albums, genres
 print('{} tracks, {} artists, {} albums, {} genres'.format(len(tracks_df), 
       len(artist_df['artist_id'].unique()), len(album_df), sum(genres_df['#tracks'] > 0)))
 #%%
+#track_cols = tracks_df.columns
+#percent_missing_tracks = tracks_df.isnull().sum()*100/len(tracks_df)
+#missing_value_tracks_df = pd.DataFrame({'ColName': track_cols,
+#                                        'percent_missing': percent_missing_tracks})
 
+def missing_values_plotter(x):
+    track_cols = x.columns
+    percent_missing_tracks = x.isnull().sum()*100/len(x)
+    missing_value_df = pd.DataFrame({'ColName': track_cols,
+                                            'percent_missing': percent_missing_tracks})
+    g = sns.barplot(missing_value_df[missing_value_df['percent_missing'] > 0]['ColName'], 
+                missing_value_df[missing_value_df['percent_missing'] > 0]['percent_missing'])
+
+    for item in g.get_xticklabels():
+        item.set_rotation(90)
+    
+
+missing_values_plotter(tracks_df)
+      
+#%%
