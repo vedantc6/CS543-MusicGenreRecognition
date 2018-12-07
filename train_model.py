049b6ae7ad7b52ed865d007dc4903cf6c56ca37a
vedantc6 <vedantc6@gmail.com>
2018-12-06 23:49:26 -0500

    updated code structure

--- train_model.py
+++ train_model.py
@@ -3,56 +3,58 @@
 """
 Created on Mon Nov 19 17:22:16 2018
 
-@author: vedantc6
+@author: Vedant Choudhary and Aditya Vyas
+@affiliation: Rutgers University, New Brunswick
 """
-#%%
-from common import GENRES, DATA_DIR
+from util import GENRES, DATA_DIR, MODEL_DIR
 from sklearn.model_selection import train_test_split
 import pickle
 import numpy as np
 from tensorflow.python.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 from tensorflow.python.keras.models import Model
-from tensorflow.python.keras.optimizers import Adam
+from tensorflow.python.keras.optimizers import Adam, RMSprop
 from tensorflow.python.keras import backend as K
 from tensorflow.python.keras.layers import Input, Conv1D, MaxPooling1D, Activation, Dropout, \
         BatchNormalization, Dense, Lambda, TimeDistributed
-
-EPOCHS = 10
+        
+EPOCHS = 50
 SEED = 42
 N_LAYERS = 3
 FILTER_LENGTH = 5
 CONV_FILTER_COUNT = 256
 BATCH_SIZE = 32
+numFeatures = 128
+LSTM_COUNT = 256
 
-#%%
 def trainModel(data, model_path):
     X = data['X']
     y = data['y']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state=SEED)
-    
+    (X_train, X_val, y_train, y_val) = train_test_split(X, y, test_size = 0.3, random_state=SEED)
+
     print('Building model...')
 
-    numFeatures = X_train.shape[2]
-#    Input shape will have different dimensional vectors, but with 128 as the batch size
-    inputShape = (None, numFeatures)
-    
-    model_input = Input(inputShape, name="input")
+    n_features = numFeatures
+    input_shape = (None, n_features)
+    model_input = Input(input_shape, name='input')
     layer = model_input
     for i in range(N_LAYERS):
-        layer = Conv1D(filters=CONV_FILTER_COUNT,
-                       kernel_size=FILTER_LENGTH,
-                       name='convolution_' + str(i+1))(layer)
+        # second convolutional layer names are used by extract_filters.py
+        layer = Conv1D(
+                filters=CONV_FILTER_COUNT,
+                kernel_size=FILTER_LENGTH,
+                name='convolution_' + str(i + 1)
+                )(layer)
         layer = BatchNormalization(momentum=0.9)(layer)
         layer = Activation('relu')(layer)
         layer = MaxPooling1D(2)(layer)
         layer = Dropout(0.5)(layer)
-    
+
     layer = TimeDistributed(Dense(len(GENRES)))(layer)
     time_distributed_merge_layer = Lambda(
-            function=lambda x: K.mean(x, axis=1), 
-            output_shape=lambda shape: (shape[0],) + shape[2:],
-            name='output_merged'
-        )
+                function=lambda x: K.mean(x, axis=1),
+                output_shape=lambda shape: (shape[0],) + shape[2:],
+                name='output_merged'
+                )
     layer = time_distributed_merge_layer(layer)
     layer = Activation('softmax', name='output_realtime')(layer)
     model_output = layer
@@ -62,27 +64,26 @@ def trainModel(data, model_path):
             loss='categorical_crossentropy',
             optimizer=opt,
             metrics=['accuracy']
-        )
-    
+            )
+
     print('Training...')
     model.fit(
-        X_train, y_train, batch_size=BATCH_SIZE, nb_epoch=EPOCHS,
-        validation_data=(X_test, y_test), verbose=1, callbacks=[
+            X_train, y_train, batch_size=BATCH_SIZE, epochs=EPOCHS,
+            validation_data=(X_val, y_val), verbose=1, callbacks=[
             ModelCheckpoint(
-                model_path, save_best_only=True, monitor='val_acc', verbose=1
-            ),
+                    model_path, save_best_only=True, monitor='val_acc', verbose=1
+                    ),
             ReduceLROnPlateau(
-                monitor='val_acc', factor=0.5, patience=10, min_delta=0.01,
-                verbose=1
+                    monitor='val_acc', factor=0.5, patience=10, min_delta=0.01,
+                    verbose=1
+                    )
+            ]
             )
-        ]
-    )
 
-    return model
-    
-#%%
-trainModel(data, DATA_DIR + "model.h5")
+  return model
 
-#%%
-with open(DATA_DIR + "data.pkl", "rb") as f:
-    data = pickle.load(f)
+if __name__ == "__main__":
+    with open(DATA_DIR + "finalé.pkl", "rb") as f:
+        data = pickle.load(f)
+
+    trainModel(data, MODEL_DIR + "model.h5")
--- util.py
+++ util.py
@@ -3,17 +3,23 @@
 """
 Created on Sun Nov 18 23:12:12 2018
 
-@author: vedantc6
+@author: Vedant Choudhary and Aditya Vyas
+@affiliation: Rutgers University, New Brunswick
 """
 import numpy as np
 import librosa as lbr
+import librosa.display
+import matplotlib.pyplot as plt
+import os
 
-METADATA_DIR = "/home/vedantc6/Desktop/Projects/CS543-MusicGenreRecognition/Data/fma_metadata/"
-AUDIO_DIR = "/home/vedantc6/Desktop/Projects/CS543-MusicGenreRecognition/Data/fma_small/"
-DATA_DIR = "/home/vedantc6/Desktop/Projects/CS543-MusicGenreRecognition/Data/"
-MAIN_DIR = "/home/vedantc6/Desktop/Projects/CS543-MusicGenreRecognition/"
+MAIN_DIR = os.path.dirname(os.path.abspath(__file__))
+METADATA_DIR = MAIN_DIR + "/Data/fma_metadata/"
+AUDIO_DIR = MAIN_DIR + "/Data/fma_small/"
+DATA_DIR = MAIN_DIR + "/Data/"
+PICKLE_DIR = MAIN_DIR + "/PickleData/"
+MODEL_DIR = MAIN_DIR + "/Models/"
 
-GENRES = ['Electronic', 'Experimental', 'Folk', 'Hip-Hop', 'Instrumental', 
+GENRES = ['Electronic', 'Experimental', 'Folk', 'Hip-Hop', 'Instrumental',
           'International', 'Pop', 'Rock']
 
 # Some hard-coded variables, which can be adjusted as per requirement
@@ -32,25 +38,23 @@ MEL_KWARGS = {
 def load_track(filename, forceShape=None):
     sample_input, sample_rate = lbr.load(filename, mono=True)
     features = lbr.feature.melspectrogram(sample_input, **MEL_KWARGS).T
-    
+    print(features.shape)
     if forceShape is not None:
         if features.shape[0] < forceShape[0]:
             delta_shape = (forceShape[0] - features.shape[0], forceShape[1])
             features = np.append(features, np.zeros(delta_shape), axis=0)
         elif features.shape[0] > forceShape[0]:
             features = features[: forceShape[0], :]
-    
+
     features[features == 0] = 1e-6
-    
+
     return (np.log(features), float(sample_input.shape[0]) / sample_rate)
 
-## USING LIBROSA EXAMPLE
-#y, sr = librosa.load(AUDIO_DIR + "001/001039.mp3", duration=0.1)
-#
-#S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128)
-#
-#plt.figure(figsize=(10, 4))
-#librosa.display.specshow(librosa.power_to_db(S, ref=np.max), y_axis='mel', x_axis='time')
-#plt.colorbar(format='%+2.0f dB')
-#plt.title('Mel spectrogram')
-#plt.tight_layout()
+# if __name__ == "__main__":
+#     # USING LIBROSA EXAMPLE
+#     y, sr = load_track(AUDIO_DIR + "000/000002.mp3")
+#     plt.figure(figsize=(10, 4))
+#     lbr.display.specshow(y, y_axis='mel', x_axis='time')
+#     plt.colorbar(format='%+2.0f dB')
+#     plt.title('Mel spectrogram')
+#     plt.tight_layout()
